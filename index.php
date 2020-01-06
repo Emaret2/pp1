46fc581f1be7a9924373c93dfc36aa5ff8de5cec
Elijah Maret <Emaret2@mail.greenriver.edu>
2020-01-06 14:39:33 -0500

    Added index page

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+    /*
+     * Elijah Maret
+     * IT 328
+     * 1/6/2020
+     * http://emaret.greenriverdev.com/328/pp1/
+     * Pair Program 1
+     */
+
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
